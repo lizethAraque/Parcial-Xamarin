457d1245272999a12c6f5643c490cf9ac886437c
Lizeth Araque <lizethgio57@gmail.com>
2021-04-30 12:15:15 -0500

    Created the singleton

--- App4/App4/MainPage.xaml.cs
+++ App4/App4/MainPage.xaml.cs
@@ -1,14 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xamarin.Forms;
+using App4.Services;
+using System.IO;
 
 namespace App4
 {
     public partial class MainPage : ContentPage
     {
+        static BikeDatabase database;
+
+        // Create the database connection as a singleton.
+        static BikeDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new BikeDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
         public MainPage()
         {
             InitializeComponent();
